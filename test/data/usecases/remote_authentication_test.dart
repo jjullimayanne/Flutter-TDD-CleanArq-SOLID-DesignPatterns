59ae06fa9bc28d03572f1f16d8d138cc6a5118bc
Julli Mayanne <jullimayanne9@gmail.com>
2022-07-26 19:21:46 -0300

    feat: teste values

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -11,23 +11,25 @@ class RemoteAuthentication {
     @required this.httpClient,
     @required this.url,
   });
-
-  //garantir que o remote chame com a url correta 
+  //garantir que o remote chame com a url correta
   Future<void> auth() async {
-    await httpClient.request(url: url);
+    await httpClient.request(url: url, method: 'POST');
   }
 }
 
 //definir class abstrata que recebe um parametro obrigatorio
 abstract class HttpClient {
-  Future<void> request({@required String url}) {}
+  Future<void> request({
+    @required String url,
+    @required String method,
+  }) {}
 }
 
 // a classe precisa ser implementada da abstrata para que possa ser testada
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  test('should call httpclient with corret URL ', () async {
+  test('should call httpclient with corret values ', () async {
     // arrange
     final httpClient = HttpClientSpy();
     final url = faker.internet.httpUrl();
@@ -37,6 +39,9 @@ void main() {
     await sut.auth();
 
     // assert
-    verify(httpClient.request(url: url));
+    verify(httpClient.request(
+      url: url,
+      method: 'POST',
+    ));
   });
 }
